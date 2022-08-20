981e4dcf60d4faf17d19a6046d276fba5bcf10c3
Nick <nsoterop@gmail.com>
2022-08-20 16:08:51 -0400

    Add environment file

---
+++ environment.js
@@ -0,0 +1,8 @@
+const env = {
+    db_connection_string: "mongodb+srv://ASiiAdmin:admin%40ASii@cluster0.nohn5.mongodb.net/ASii_Medical?retryWrites=true",
+    SQUARE_ACCESS_TOKEN: "EAAAEHVDnZGoAV98ajFPnSlyrWZTxQF-f3-0t5dpiRngV_suGcVweOXkDw2GnS2J",
+    LOCATION_ID: "4MM15PDYYA3HY",
+    production: false
+}
+
+module.exports = env
